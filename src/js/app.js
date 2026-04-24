8ba2faae23bfbfdfa02942701ace3b19e18950c8
илья бескаравайный <ilia966@tut.by>
2026-04-24 09:46:31 +0300

    asd

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
 import * as flsFunctions from "./modules/functions.js";
 import "./modules/jquery-3.7.1.min.js";
-import "./modules/fslightbox.js";
+import { Fancybox } from "./modules/fancybox.esm.js";
+import "./modules/select2.min.js";
 import "./modules/bootstrap.bundle.min.js";
 import './components.js';
 
 flsFunctions.isWebp();
+
+Fancybox.bind("[data-fancybox]", {
+  closeButton: false,
+});
 
 // Import swiper
 import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
@@ -23,4 +28,44 @@ new Swiper($('.headerMarquee')[0], {
   allowTouchMove: false,
   watchSlidesProgress: true,
   a11y: false,
+});
+
+
+const actionButton = document.querySelector('.footerActionBtn');
+actionButton?.addEventListener('click', () => {
+  setTimeout(function () {
+    // Прокручиваем к самому низу документа
+    window.scrollTo({
+      top: document.body.scrollHeight, // Прокрутить до конца документа
+      behavior: 'smooth' // Плавная анимация прокрутки
+    });
+  }, 300); // 1000 миллисекунд = 1 секунда
+});
+
+window.addEventListener('resize', () => {
+  $('.select-field').select2({
+    maximumInputLength: 20 // only allow terms up to 20 characters long
+  });
+});
+
+$(document).ready(function () {
+  $('.select-field').select2({
+    maximumInputLength: 20 // only allow terms up to 20 characters long
+  });
+});
+
+let sortBtn = document.querySelector('.sortBtn');
+let sortBlock = document.querySelector('.sortBlock');
+sortBtn.addEventListener('click', () => {
+  sortBlock.classList.toggle('active');
+});
+
+document.addEventListener('click', e => {
+	let target = e.target;
+	let its_sort_btn = target == sortBtn || sortBtn.contains(target);
+	let its_sort_block = target == sortBlock || sortBlock.contains(target);
+
+	if (!its_sort_btn && !its_sort_block) {
+		sortBlock.classList.remove('active');
+	}
 });
