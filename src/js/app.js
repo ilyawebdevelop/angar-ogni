a192e1f42ff7f62c8af2698bc4630ac5ae35a397
илья бескаравайный <ilia966@tut.by>
2026-04-30 06:36:07 +0300

    asd

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 import "./modules/jquery-3.7.1.min.js";
+import AirDatepicker from 'air-datepicker';
 import { Fancybox } from "./modules/fancybox.esm.js";
 import "./modules/select2.min.js";
 import "./modules/bootstrap.bundle.min.js";
+import "./modules/inputmask.min.js";
 import './components.js';
 
 flsFunctions.isWebp();
@@ -15,6 +17,23 @@ Fancybox.bind("[data-fancybox]", {
 import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar]);
 
+let calendarArray = document.querySelectorAll('.calendar');
+calendarArray.forEach(el => {
+  // air datepicker
+  new AirDatepicker(el, {
+    minDate: new Date(),
+    autoClose: true,
+    position: 'bottom right',
+  });
+
+  Inputmask("datetime", {
+    inputFormat: "dd.mm.yyyy",
+    placeholder: "ДД.ММ.ГГГГ",
+    leapday: "29-02-",
+    separator: ".",
+    alias: "dd/mm/yyyy"
+  }).mask(el);
+});
 
 new Swiper($('.headerMarquee')[0], {
   slidesPerView: 'auto',
@@ -29,7 +48,6 @@ new Swiper($('.headerMarquee')[0], {
   watchSlidesProgress: true,
   a11y: false,
 });
-
 
 const actionButton = document.querySelector('.footerActionBtn');
 actionButton?.addEventListener('click', () => {
@@ -56,16 +74,57 @@ $(document).ready(function () {
 
 let sortBtn = document.querySelector('.sortBtn');
 let sortBlock = document.querySelector('.sortBlock');
-sortBtn.addEventListener('click', () => {
+sortBtn?.addEventListener('click', () => {
   sortBlock.classList.toggle('active');
 });
 
 document.addEventListener('click', e => {
-	let target = e.target;
-	let its_sort_btn = target == sortBtn || sortBtn.contains(target);
-	let its_sort_block = target == sortBlock || sortBlock.contains(target);
+  let target = e.target;
+  let its_sort_btn = target == sortBtn || sortBtn?.contains(target);
+  let its_sort_block = target == sortBlock || sortBlock?.contains(target);
 
-	if (!its_sort_btn && !its_sort_block) {
-		sortBlock.classList.remove('active');
-	}
+  if (!its_sort_btn && !its_sort_block) {
+    sortBlock?.classList.remove('active');
+  }
+});
+
+// Инициализация слайдера galSlider
+document.querySelectorAll('.articleSlider').forEach(n => {
+  const mySwiperArticleGal = new Swiper(n, {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    speed: 600,
+    loop: true,
+    // autoplay: true,
+    navigation: {
+      prevEl: n.querySelector('.navArrowPrev'),
+      nextEl: n.querySelector('.navArrowNext'),
+    },
+    thumbs: { // указываем на превью слайдер
+      swiper: {
+        el: n.closest('.sliderW').querySelector('.articleThumbSlider'),
+        slidesPerView: 1,
+        spaceBetween: 0,
+        speed: 600,
+        loop: true,
+        watchSlidesProgress: true,
+      }
+    },
+  });
+});
+
+// search clear
+let searchInputArray = document.querySelectorAll('.formInput--search');
+searchInputArray.forEach(el => {
+  let clearValue = el.closest('.searchW').querySelector('.search-clear');
+  el?.addEventListener('input', (event) => {
+    clearValue?.classList.add('active');
+  });
+
+  clearValue?.addEventListener('click', () => {
+    el.value = '';
+    el.focus();
+    clearValue?.classList.remove('active');
+  });
+
 });
